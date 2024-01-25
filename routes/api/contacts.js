884f085e3b826f5449d3eaef96630855892156d3
Rublevska <73138546+Rublevska@users.noreply.github.com>
2024-01-25 23:38:46 +0200

    done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,19 +1,31 @@
 const express = require("express");
 
 const ctrl = require("../../controllers/contacts");
-const { validateBody } = require("../../middlewares");
-const schemas = require("../../schemas/contacts");
+const { validateBody, isValidId } = require("../../middlewares");
+const { schemas } = require("../../models/contact");
 
 const router = express.Router();
 
 router.get("/", ctrl.getAll);
 
-router.get("/:contactId", ctrl.getById);
+router.get("/:contactId", isValidId, ctrl.getById);
 
 router.post("/", validateBody(schemas.addSchema), ctrl.add);
 
-router.delete("/:contactId", ctrl.remove);
+router.delete("/:contactId", isValidId, ctrl.remove);
 
-router.put("/:contactId", validateBody(schemas.addSchema), ctrl.update);
+router.put(
+  "/:contactId",
+  isValidId,
+  validateBody(schemas.updateSchema, "Body must have at least one field"),
+  ctrl.update
+);
+
+router.patch(
+  "/:contactId/favorite",
+  isValidId,
+  validateBody(schemas.updateFavoriteSchema, "missing field favorite"),
+  ctrl.updateStatusContact
+);
 
 module.exports = router;
